9bf20dbc6513605983ed1d3d45ddb97909a93f95
PauloAlexandreSantos <tecnicopaulo@outlook.pt>
2021-05-16 15:03:12 +0100

    sldk

--- app/Http/Controllers/Site/HomeController.php
+++ app/Http/Controllers/Site/HomeController.php
@@ -3,14 +3,12 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
-use App\Models\ClasseDisciplina;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(ClasseDisciplina $data)
+    public function index()
     {
-        $response['cds'] = $data->classes_disciplinas()->get();
         $response['uri'] = 'ops';
         return view('site.index', $response);
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,42 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Cabecalho::factory(1)->create();
+        //\App\Models\Cabecalho::factory(1)->create();
         \App\Models\User::factory(1)->create();
          // \App\Models\Aluno::factory(10)->create();
         // \App\Models\Candidatura::factory(150)->create();
-         
-         // DB::table('alunos')->insert([
-         //     'nome' => Str::random(10)        
-         // ]);
-       /*  DB::table('candidatos')->insert([
-              'nome' => Str::random(10)        
-         ]);*/
-         $dias = Dias_semanas::count();
-         if($dias == 0)
-         {
-            Dias_semanas::create([
-                'vc_dia'=>'Segunda-Feira',
-            ]);
-            Dias_semanas::create([
-                'vc_dia'=>'Terça-Feira',
-            ]);
-            Dias_semanas::create([
-                'vc_dia'=>'Quarta-Feira',
-            ]);
-            Dias_semanas::create([
-                'vc_dia'=>'Quinta-Feira',
-            ]);
-            Dias_semanas::create([
-                'vc_dia'=>'Sexta-Feira',
-            ]);
-            Dias_semanas::create([
-                'vc_dia'=>'Sábado',
-            ]);
-            Dias_semanas::create([
-                'vc_dia'=>'Domingo',
-            ]);
-         }
+
          
     }
 }
